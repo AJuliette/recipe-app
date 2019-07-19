df7c1f51a42a7554092671bbb554c150621808b6
Juliette Audema <juliette.audema@gmail.com>
2019-07-19 11:15:15 +0200

    First display of recipes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = `${process.env.REACT_APP_API_ID}`
+  const APP_KEY = `${process.env.REACT_APP_API_KEY}`
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    } 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button 
+          className="search-button"
+          type="submit"
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </button>
+      </form>
+      {recipes.map(recipe =>(
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image} 
+        />
+      ))}
     </div>
   );
 }
